686a3b1d7e42bc9d91b350f81fa91b722fbfaee9
Scoty Stobbe <scotystobbe@gmail.com>
2026-01-21 13:41:24 -0700

    tweak working

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           className="wordmark-header"
         />
         <p className="subtitle">
-          Build, deconstruct, and rebuild<br className="mobile-break" /> alternate MOCs with ease!
+          Build, deconstruct, and<br className="mobile-break" />  rebuild Alternate MOCs with ease!
         </p>
       </header>
 
@@ -29,18 +29,23 @@ function App() {
           <div className="description-section">
             <h3>Why I Made It</h3>
             <p className="description">
-              One of the coolest things about LEGO is that you can take them apart and rebuild them endlessly. 
+              One of the coolest things about LEGO is that you can take the bricks apart and rebuild them endlessly. 
               Just six of the classic 2x4 LEGO bricks in the same color can be assembled in 915 million unique combinations.
             </p>
             <p className="description">
               If you're like me, and you love building and re-building LEGO sets, but don't quite have the master builder creativity to build 
               from scratch, <strong>Alternate MOCs</strong> are an awesome solution. An Alternate MOC is a custom, 
               fan-designed model built using only the pieces from a single official LEGO set, rearranged into 
-              a completely new creation.
+              a new creation.
             </p>
             <p className="description">
-              <strong>MOCFlow</strong> provides structure and guidance to ease the process of building, deconstructing, and 
-              rebuilding, especially for larger sets where managing pieces becomes challenging.
+            As I started building more Alternate MOCs, I noticed an area that could use some help to ease the process of building, deconstructing,
+            and rebuilding. When you build the official set first, you will then need to tear it all down in order to build the Alternate MOC.
+            If it's a smaller set, this isn't a big deal. You can fairly easily find the pieces you need in a pile of 300 or so LEGO pieces.
+            But if you're working with a set of 1,000+ pieces this quickly becomes unmanageable.
+            </p>
+            <p className="description">
+              <strong>MOCFlow</strong> provides tools, structure, and guidance to ease this process, especially for larger sets where managing and finding pieces becomes challenging.
             </p>
           </div>
 
